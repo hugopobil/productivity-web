ea3aaa70d5cff4b1517b3a2d8a51ddb3fdc7103e
Hugo Pobil <hpascual@mrhouston.net>
2024-02-21 17:48:50 +0100

    initial-push

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple home view in React.</p>
+    </div>
+  );
+};
+
+export default Home;
