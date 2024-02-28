8424e77067c9158f62d3d44c3485bbe524bbff2e
Hugo Pobil <hpascual@mrhouston.net>
2024-02-28 10:26:55 +0100

    update

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,11 +1,23 @@
-import './TitleBar.css'
+import "./TitleBar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../contexts/AuthContext";
 
 const TitleBar = () => {
-    return (
-        <div className='TitleBar'>
-            <h1>Achieve It</h1>
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="TitleBar">
+      <h1>ACHIEVE IT</h1>
+      {user && (
+        <div className="TitleBar-item">
+          <Link to="/profile" className="TitleBar__link">
+            {user.username}
+          </Link>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default TitleBar;
