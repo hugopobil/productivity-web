0a16b85573956900c8a5dda4e49c5492b65033a0
Hugo Pobil <hpascual@mrhouston.net>
2024-03-02 13:21:48 +0100

    update

---
+++ src/components/PostsByUser.jsx
@@ -0,0 +1,17 @@
+import "./PostByUser.css";
+
+const PostsByUser = ({ postData }) => {
+
+
+
+  return (
+    <div className="post-by-user-container">
+      <h3 className="post-by-user-title">{postData.title}</h3>
+      <p className="post-by-user-content">{postData.location}</p>
+      <p className="post-by-user-content">{postData.content}</p>
+      <img className="post-by-user-image" src={postData.image} alt="" />
+    </div>
+  );
+};
+
+export default PostsByUser;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,22 +1,41 @@
 import AuthContext from "../contexts/AuthContext";
-import { useContext } from "react";
-import OnePost from "../components/OnePost";
+import { useContext, useEffect, useState } from "react";
+import { getUserPosts } from "../services/PostService";
+import { Link } from "react-router-dom";
+import "./profile.css";
+import PostsByUser from "../components/PostsByUser";
 
 const Profile = () => {
   const { user } = useContext(AuthContext);
+  const [userPosts, setUserPosts] = useState([]);
+
+  const numPosts = userPosts.length;
+
+  useEffect(() => {
+    getUserPosts(user.id).then((response) => {
+      setUserPosts(response);
+    });
+  }, []);
 
   return (
-    <div>
-      <h1>Profile</h1>
-        <p>User: {user.username}</p>
-        <p>Email: {user.email}</p>
-        
-        <h2>Liked Posts</h2>
-        <ul>
+    <div className="profile-main-page">
+      <div className="profile-user-main-info">
+        <img src={user.image} alt="userImage" />
+        <h2>{user.username}</h2>
+      </div>
+      <div className="posts-info">
+      <p>Number of posts: {numPosts}</p>
+      </div>
+
+      {userPosts.map((post) => {
+        return <PostsByUser postData={post} />;
+      })}
+     
+      {/* <ul>
           {user.likes.map((like) => {
             return <OnePost postData={like.post} />;
           })}
-        </ul>
+        </ul> */}
     </div>
   );
 };
